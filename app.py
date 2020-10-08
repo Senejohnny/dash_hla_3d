d99878f8d6ecf909cdae53dbfb05c3cd25bc203f
Danial <d.senejohnny@gmail.com>
2020-10-08 16:26:07 +0200

    This commit finalizes the 2nd Sprint

--- app.py
+++ app.py
@@ -29,8 +29,11 @@ app = dash.Dash(__name__, title='HLA 3D Viewer', external_stylesheets=external_s
 # })
 
 epitope_db, desa_db = load_data()
+
 desa_db = desa_db[desa_db.Status=='DESA']
-hlas, _3d_data, _hlavsdesa, hlavsdesa = data_3dviewer(desa_db, epitope_db, 4080)
+Tx = 4606
+
+hlas, _3d_data, _hlavsdesa, hlavsdesa = data_3dviewer(desa_db, epitope_db, Tx)
 
 hla_1, component_1 = div_3dviewer(hlas, 0, _3d_data, _hlavsdesa)
 hla_2, component_2 = div_3dviewer(hlas, 1, _3d_data, _hlavsdesa)
@@ -42,7 +45,7 @@ hla_6, component_6 = div_3dviewer(hlas, 5, _3d_data, _hlavsdesa)
 
 desa_db_table = desa_db[['TransplantID', 'Status', '#DESA', 'FailureCode_NOTR', 'GraftSurvivalYears_NOTR']]
 desa_db_table['DESA->Donor_HLA'] = desa_db['DESA->Donor_HLA'].apply(lambda x: str(dict(x)))
-
+desa_db_table = desa_db_table.sort_values(by='#DESA', ascending=False)
 # ######################################################################
 # App Layout
 # ######################################################################
@@ -87,22 +90,48 @@ app.layout = html.Div([
                             ),
                             html.Div(id='output-desa-upload'),
                         ]),
+                    html.H5("Choose Tx for 3d View:"),
+                    dcc.Input(
+                        id="transplant_3d_view", 
+                        placeholder="Enter Tx number'",
+                        type='text',
+                        value=''
+                    )
                     ], className="four columns"),
                     html.H1(""),
                     html.Div(id='output', 
                             children=dash_table.DataTable(
-                                id='table_1',
+                                id='table',
                                 columns=[{"name": i, "id": i} for i in desa_db_table.columns],   
                                 data=desa_db_table.to_dict('records'),
-                                style_table={'height': '720px', 'overflowY': 'auto'},
-                            ), className="eight columns")
+                                page_size= 20,
+                                editable=True,
+                                style_table={'height': '400px', 
+                                             'overflowY': 'auto'},
+                                style_cell_conditional=[
+                                                        {
+                                                            'if': {'column_id': c},
+                                                            'textAlign': 'left'
+                                                        } for c in ['Date', 'Region']
+                                                    ],
+                                style_data_conditional=[
+                                                        {
+                                                            'if': {'row_index': 'odd'},
+                                                            'backgroundColor': 'rgb(248, 248, 248)'
+                                                        }
+                                                    ],
+                                style_header={
+                                                'backgroundColor': 'rgb(230, 230, 230)',
+                                                'fontWeight': 'bold'
+                                            }
+                            ), className="eight columns"),  # end of table
             ]), 
             dcc.Tab(label='3D View', 
                     value='tab-2',
                     children=html.Div([
                         html.H5('Overview'),
-                        html.Div('''
-                            This is a 3D view of an HLA with DESA color coded  
+                        html.Div(f'''
+                            This is a 3D view of HLA molecules in Transplant ID: {Tx}, with DESA color depicted in yellow  
                         '''),
                         dcc.Loading(
                             type="circle",
@@ -110,34 +139,34 @@ app.layout = html.Div([
                             html.Div(      # HLA 1
                                 id='mol3d-viewer-1',
                                 children=[
-                                html.H4(f'HLA: {hla_1}'),
+                                html.H5(f'HLA: {hla_1}'),
                                 # html.P(f'DESA: {hlavsdesa_short[hla_1]}'),
                                 component_1,
                             ], className="four columns"),
                             html.Div(      # HLA 2
                                 id='mol3d-viewer-2',
                                 children=[
-                                html.H4(f'HLA: {hla_2}'),
+                                html.H5(f'HLA: {hla_2}'),
                                 # html.P(f'DESA: {hlavsdesa_short[hla_2]}'),
                                 component_2,
                             ], className="four columns"),
                             html.Div(      # HLA 3
                                 id='mol3d-viewer-3',
                                 children=[
-                                html.H4(f'HLA: {hla_3}'),
+                                html.H5(f'HLA: {hla_3}'),
                                 # html.P(f'DESA: {hlavsdesa_short[hla_3]}'),
                                 component_3
                             ], className="four columns"),
                             html.Div(      # HLA 4
                                 id='mol3d-viewer-4',
                                 children=[
-                                html.H4(f'HLA: {hla_4}'),
+                                html.H5(f'HLA: {hla_4}'),
                                 component_4
                             ], className="four columns"),
                             html.Div(      # HLA 5
                                 id='mol3d-viewer-5',
                                 children=[
-                                html.H4(f'HLA: {hla_5}'),
+                                html.H5(f'HLA: {hla_5}'),
                                 component_5
                             ], className="four columns"),
                             html.Div(      # HLA 6
@@ -177,7 +206,7 @@ app.layout = html.Div([
             )
         ]),
         # Hidden div inside the app that stores the intermediate value
-        html.Div(id='hidden-desa-data', style={'display': 'none'}),
+        html.Div(id='text-output', children='')#, style={'display': 'none'}),
     ]),
 ],)
 # ######################################################################
@@ -202,6 +231,34 @@ app.layout = html.Div([
 #     else:
 #         return no_update,  html.Div(['No data is uploaded'])
 
+@app.callback(Output('text-output', 'children'),
+            [Input('transplant_3d_view', 'value')],)
+def get_active_cell_value (value):
+    if value:
+        return value
+    else:
+        return no_update
+
+def _output(hla, component):
+    return html.H4(f'HLA: {hla_6}'), component
+
+# @app.callback(Output('mol3d-viewer-6', 'children'),
+#             [Input('transplant_3d_view', 'value')],)
+# def get_tx_3d(Tx):
+#     print(Tx)
+#     if Tx:
+#         print('hi')
+#         hlas, _3d_data, _hlavsdesa, hlavsdesa = data_3dviewer(desa_db, epitope_db, Tx)
+#         print(Tx, hlavsdesa)
+#         hla_1, component_1 = div_3dviewer(hlas, 0, _3d_data, _hlavsdesa)
+#         # hla_2, component_2 = div_3dviewer(hlas, 1, _3d_data, _hlavsdesa)
+#         # hla_3, component_3 = div_3dviewer(hlas, 2, _3d_data, _hlavsdesa)
+#         # hla_4, component_4 = div_3dviewer(hlas, 3, _3d_data, _hlavsdesa)
+#         # hla_5, component_5 = div_3dviewer(hlas, 4, _3d_data, _hlavsdesa)
+#         # hla_6, component_6 = div_3dviewer(hlas, 5, _3d_data, _hlavsdesa)
+#         return _output(hla_1, component_1)
+#     else:
+#         return no_update
 
 if __name__ == '__main__':
     if Repository('.').head.shorthand == 'master':
